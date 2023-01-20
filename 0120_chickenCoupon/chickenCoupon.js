19f8dcde96c30f8d2a050c3ddfe040a9aaa71468
Nyes98 <111405218+Nyes98@users.noreply.github.com>
2023-01-20 12:36:43 +0900

    Create chickenCoupon.js

---
+++ 0120_chickenCoupon/chickenCoupon.js
@@ -0,0 +1,9 @@
+function solution(chicken) {
+  let answer = parseInt(chicken / 10);
+  let coupon = parseInt(chicken / 10) + (chicken % 10);
+  while (coupon > 9) {
+    answer += parseInt(coupon / 10);
+    coupon = parseInt(coupon / 10) + (coupon % 10);
+  }
+  return answer;
+}
